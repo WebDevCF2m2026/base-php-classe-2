96783099f723c84522e7c43c85653e3cfea0f26e
Bruce-Wuthrich <azertytagada8@gmail.com>
2026-02-19 15:19:41 +0100

    exercice 6-7

---
+++ Stagiaires/BruceW/06-declarer des variables.php
@@ -0,0 +1,38 @@
+<?php
+
+$nom = "Alice Dupont";
+
+
+$age = 28;
+
+$taille = 1.68;
+
+$estEtudiant = true;
+
+$fruits = ["pomme", "banane", "kiwi", "fraise"];
+
+$adresse = null;
+
+echo "<pre>";
+
+var_dump($nom);
+echo "\n";
+
+var_dump($age);
+echo "\n";
+
+var_dump($taille);
+echo "\n";
+
+var_dump($estEtudiant);
+echo "\n";
+
+var_dump($fruits);
+echo "\n";
+
+var_dump($adresse);
+echo "\n";
+
+echo "</pre>";
+
+?>
