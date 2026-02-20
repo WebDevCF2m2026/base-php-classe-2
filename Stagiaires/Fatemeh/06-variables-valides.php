24dfff526e456cf75851320ecabb64d40471895f
fatemehmjz <mjznazi@gmail.com>
2026-02-20 13:32:30 +0100

    new

--- Stagiaires/Fatemeh/06-variables-valides.php
+++ Stagiaires/Fatemeh/06-variables-valides.php
@@ -1,24 +1,22 @@
 <?php
-// string (chaîne de caractères)
-$nom_de_variable = "valeur de la variable";
+// String
+$nom = "Fatemeh";
+// Int
+$age = 33; 
+// Float
+$prix = 10.5; 
+// Boolean
+$Valide = true; 
+// Array
+$alpha = ["A", "BCD"]; 
+// Null
+$rien = null; 
 
-// integer (nombre entier)
-$nomDeVariable2 = 123;
-
-// float (nombre décimal)
-$NomDeVariable3 = 123.456;
-
-// boolean (vrai ou faux)
-$estActif = true; // ou false
-
-// array (tableau indexé)
-$nombres = [1, 2, 3];
-
-// array (tableau associatif)
-$personne = ["clef1" => "valeur1", "clef2" => "valeur2"];
-
-// null (absence de valeur)
-$rien = null;
-
-// Affichage avec le débogueur var_dump()
-var_dump($nom_de_variable, $nomDeVariable2, $NomDeVariable3);
+// Affichage avec var_dump
+var_dump($nom);
+var_dump($age);
+var_dump($prix);
+var_dump($Valide);
+var_dump($alpha);
+var_dump($rien);
+?>
