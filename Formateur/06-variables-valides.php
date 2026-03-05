1c456eac6a2898ccaa3b315cf7459982c7a68216
mikhawa <michaeljpitz@gmail.com>
2026-03-05 12:49:39 +0100

    exe 06 et 007

---
+++ Formateur/06-variables-valides.php
@@ -0,0 +1,18 @@
+<?php
+
+$nom = "Mike";
+$age = 30;
+$taille = 1.75;
+$estEtudiant = true;
+$cours = ["Math", "Physique", "Chimie"];
+$professeur = [
+    "titre" => "Mr Pitz",
+    "matiere" => "Informatique",
+];
+$etudiants = [
+    ["nom" => "Alice", "age" => 22],
+    ["nom" => "Bob", "age" => 24],
+    ["nom" => "Charlie", "age" => 21]
+];
+$description = null;
+var_dump($nom, $age, $taille, $estEtudiant, $cours, $professeur, $etudiants, $cours, $description);
