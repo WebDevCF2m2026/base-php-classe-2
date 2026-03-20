5140c907943377096879de3de8e210a1a157e4f8
iren-savastre <aana2829@yahoo.com>
2026-03-20 11:17:24 +0100

    create .gitignore with config.php protected

---
+++ Stagiaires/Irina/public/index.php
@@ -0,0 +1,44 @@
+<?php
+#Formateur/19-front-controller/index.php
+
+//fichier de redirection vers public
+//inutile so on a bien fait pointer
+//un hote virtuel  ou un nom de domaine
+//vers public
+header("location: public");
+//securite pour etre certain d'arreter le serveur PHP,bonne pratique
+exit;
+'''
+- Creation du controleur frontal.
+
+### public/index.php -> Front Controller
+
+Ce fichier est ceui ou **TOUTES les requetes http vers les pages vont transiter**.
+
+Pour voir le chemin, on peut utiliser :
+```php
+//contantes magiques __DIR__,
+//nous donne le chemin du serveur depuis la racine
+echo__DIR__;
+
+
+
+
+///index 2
+
+<?php
+# <Formateur/19-front-controller/public/index.php
+
+/********************************************** 
+ * **
+ * **             CONTROLER FRONTAL 
+ * 
+ * Ce fichier va gerer toutes les requetes
+ * vers les pages du site
+ * 
+ * *********************************************/
+
+// 
+// contantes magiques __DIR__, 
+// nous donne le chemin du serveur depuis la racine
+echo __DIR__;
